abb41e35990e8c1ad041c30100b488639a717774
Mostafa Sayed Abdelmonam <43078536+eng-mostafa-sayed@users.noreply.github.com>
2022-05-18 23:11:13 +0200

    home

--- src/myViews/Home.js
+++ src/myViews/Home.js
@@ -1,25 +1,36 @@
-import React, { Component } from "react";
-
+import React, { Component, useState, useEffect } from "react";
+import { getAll } from "../BooksAPI";
 import BooksShelf from "../components/BooksShelf";
-export default class Home extends Component {
-  render() {
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <BooksShelf title="Currently Reading" />
-          <BooksShelf title="Want to Read" />
-          <BooksShelf title="read" />
-        </div>
-        <div className="open-search">
-          <a //onClick={() => setShowSearchpage(!showSearchPage)}
-          >
-            Add a book
-          </a>
-        </div>
+export default function Home() {
+  const [books, setBooks] = useState([]);
+  const getAllBooks = async () => {
+    try {
+      const data = await getAll();
+      setBooks(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getAllBooks();
+  }, []);
+
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
       </div>
-    );
-  }
+      <div className="list-books-content">
+        <BooksShelf title="Currently Reading" />
+        <BooksShelf title="Want to Read" />
+        <BooksShelf title="read" />
+      </div>
+      <div className="open-search">
+        <a //onClick={() => setShowSearchpage(!showSearchPage)}
+        >
+          Add a book
+        </a>
+      </div>
+    </div>
+  );
 }
