93fe587b110ba8fa29bc1e6487804f77ce98d002
Mostafa Sayed Abdelmonam <43078536+eng-mostafa-sayed@users.noreply.github.com>
2022-05-19 20:54:02 +0200

    fixing a bug

--- src/components/BooksShelf.js
+++ src/components/BooksShelf.js
@@ -6,7 +6,7 @@ import Books from "./Books";
 export default function BooksShelf(props) {
   return (
     <div className="bookshelf">
-      <h2 className="bookshelf-title">{props.title}</h2>
+      <h2 className="bookshelf-title">{props.sectionTitle}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
           {props.books.map((book) => (
