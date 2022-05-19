bd7509d95940ee4be1a0a15d1dcef9e824213a65
Mostafa Sayed Abdelmonam <43078536+eng-mostafa-sayed@users.noreply.github.com>
2022-05-19 17:43:15 +0200

    error

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { Component, useState, useEffect } from "react";
+import { getAll } from "./BooksAPI";
 import "./App.css";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import { useState } from "react";
 import Search from "./myViews/Search";
 import Home from "./myViews/Home";
 
 function App() {
+  const [books, setBooks] = useState([]);
+  let allBooksArray = [];
+  const getAllBooks = async () => {
+    try {
+      const data = await getAll();
+      setBooks(data);
+      console.log("data is");
+      console.log(data);
+      allBooksArray = data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAllBooks();
+  }, []);
   return (
     <div className="app">
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={<Home allBooks={allBooksArray} />} />
         <Route exact path="/search" element={<Search />} />
       </Routes>
     </div>
