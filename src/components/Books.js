51ab7da47312fe2a3df099333a4ae508cc32c704
Mostafa Sayed Abdelmonam <43078536+eng-mostafa-sayed@users.noreply.github.com>
2022-05-19 17:59:24 +0200

    img-error

--- src/components/Books.js
+++ src/components/Books.js
@@ -10,8 +10,7 @@ export default function Books(props) {
             style={{
               width: 128,
               height: 193,
-              backgroundImage:
-                'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
+              backgroundImage: props.book.imageLinks.thumbnail,
             }}
           ></div>
           <div className="book-shelf-changer">
