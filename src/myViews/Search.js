b6d34cef91f7828da1a89ac57036c0306bb8e56c
Mostafa Sayed Abdelmonam <43078536+eng-mostafa-sayed@users.noreply.github.com>
2022-05-19 18:55:09 +0200

    working on search

--- src/myViews/Search.js
+++ src/myViews/Search.js
@@ -1,21 +1,38 @@
-import React, { Component } from "react";
-
-export default class Search extends Component {
-  render() {
-    return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <a className="close-search" href="/">
-            Close
-          </a>
-          <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title, author, or ISBN" />
-          </div>
-        </div>
-        <div className="search-books-results">
-          <ol className="books-grid"></ol>
+import React, { Component, useState, useEffect } from "react";
+import { getAll } from "../BooksAPI";
+import BooksShelf from "../components/BooksShelf";
+export default function Search() {
+  const [books, setBooks] = useState([]);
+  let allBooksArray = [];
+  const getAllBooks = async () => {
+    try {
+      const data = await getAll();
+      setBooks(data);
+      console.log("data is");
+      console.log(data);
+      allBooksArray = data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAllBooks();
+  }, []);
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <a className="close-search" href="/">
+          Close
+        </a>
+        <div className="search-books-input-wrapper">
+          <input type="text" placeholder="Search by title, author, or ISBN" />
         </div>
       </div>
-    );
-  }
+      <div className="search-books-results">
+        <ol className="books-grid">
+          <BooksShelf />
+        </ol>
+      </div>
+    </div>
+  );
 }
