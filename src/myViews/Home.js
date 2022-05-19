9b9bc0b3899f6c0e5ab077613e57be4ae753d85f
Mostafa Sayed Abdelmonam <43078536+eng-mostafa-sayed@users.noreply.github.com>
2022-05-19 17:47:04 +0200

    same error

--- src/myViews/Home.js
+++ src/myViews/Home.js
@@ -1,6 +1,23 @@
 import React, { Component, useState, useEffect } from "react";
 import BooksShelf from "../components/BooksShelf";
+import { getAll } from "../BooksAPI";
 export default function Home(allBooks) {
+  const [books, setBooks] = useState([]);
+  let allBooksArray = [];
+  const getAllBooks = async () => {
+    try {
+      const data = await getAll();
+      setBooks(data);
+      console.log("data is");
+      console.log(data);
+      allBooksArray = data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAllBooks();
+  }, []);
   return (
     <div className="list-books">
       <div className="list-books-title">
@@ -8,9 +25,18 @@ export default function Home(allBooks) {
       </div>
 
       <div className="list-books-content">
-        <BooksShelf title="Currently Reading" allBooks={allBooks} />
-        <BooksShelf title="Want to Read" allBooks={allBooks} />
-        <BooksShelf title="read" allBooks={allBooks} />
+        <BooksShelf
+          sectionTitle="Currently Reading"
+          books={books.filter((book) => book.shelf == "currentlyReading")}
+        />
+        <BooksShelf
+          sectionTitle="Want to Read"
+          books={books.filter((book) => book.shelf == "wantToRead")}
+        />
+        <BooksShelf
+          sectionTitle="read"
+          books={books.filter((book) => book.shelf == "read")}
+        />
       </div>
       <div className="open-search">
         <a //onClick={() => setShowSearchpage(!showSearchPage)}
