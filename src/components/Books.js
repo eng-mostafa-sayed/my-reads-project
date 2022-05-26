ba0477a4c3ee21bbf37afd8da3964c456f0f5309
Mostafa Sayed Abdelmonam <43078536+eng-mostafa-sayed@users.noreply.github.com>
2022-05-26 16:49:32 +0200

    idk

--- src/components/Books.js
+++ src/components/Books.js
@@ -48,7 +48,7 @@ export default function Books(props) {
           </div>
         </div>
         <div className="book-title">{props.book.title}</div>
-        <div className="book-authors">{props.book.publisher}</div>
+        <div className="book-authors">{props.book.authors}</div>
       </div>
     </li>
   );
